dc47d2ad28731ec4603cc5047c687dddc9626a03
Sporesirius <sporesirius+git+public@pm.me>
2022-03-18 23:56:55 +0100

    Remove cookie

--- src/getSeats.py
+++ src/getSeats.py
@@ -3,10 +3,9 @@ import requests
 url = "https://www.cinemaxx.de/compeso/getSeats"
 
 headers = {
-  "cookie": "bookingsessionid=fbab8e4a-97ae-4355-acc2-68710df18c2a",
   "Content-Type": "application/x-www-form-urlencoded"
 }
-payload = "seance_id=A690B800023FVUVQLG"
+payload = "seance_id=7790B800023FVUVQLG"
 
 response = requests.request("POST", url, data=payload, headers=headers)
 
